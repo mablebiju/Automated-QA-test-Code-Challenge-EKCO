6e5c7764d0a1858177bccd3b287848e4dbc9e2ec
mablebiju <52173815+mablebiju@users.noreply.github.com>
2023-11-06 08:55:34 +0000

    Add files via upload

--- test1.test.js
+++ test1.test.js
@@ -1,10 +1,68 @@
-//Test Case 2 – Get single user, Update user
 import { expect } from "@playwright/test";
 import request from "supertest";
 
 const baseUrl = 'https://reqres.in/api/';
 
-//Step 1: Get – Single User
+// Step 1: Post - Register
+const payload= {
+  email: "eve.holt@reqres.in",
+  password: "pistol"
+  
+}
+describe('Register endpoint', () => {
+	it('should return a 200 status code', async () => {
+		const response = await request(baseUrl)
+			.post('register')
+      .send(payload)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json');
+
+    //console.log("response", response)
+
+		expect(response.statusCode).toBe(200);
+    expect(typeof response._body.token).toBe('string')
+    expect(typeof response._body.id).toBe('number')
+
+	});
+})
+
+
+//Step 2: Post - Login
+
+
+describe('Loging endpoint', () => {
+	it('should return a 200 status code', async () => {
+		const response = await request(baseUrl)
+			.post('login')
+      .send(payload)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json');
+
+    // console.log("response", response)
+
+		expect(response.statusCode).toBe(200);
+    expect(typeof response._body.token).toBe('string')
+
+	});
+});
+
+
+// Step 3: Delete
+describe('Delete endpoint', () => {
+	it('should return a 204 status code', async () => {
+		const response = await request(baseUrl)
+			.delete('delete')
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json');
+
+    //console.log("response", response)
+
+		expect(response.statusCode).toBe(204);
+
+	});
+})
+
+/*
 describe('Single User Endpoint', () => {
 	it('should return a 200 status code and valid response body', async () => {
 		const response = await request(baseUrl)
@@ -24,8 +82,6 @@ describe('Single User Endpoint', () => {
 	});
 });
 
-
-//Step 2: PUT – Update
 const payload = {
   name: "morpheus",
   job: "zion resident"
@@ -45,5 +101,4 @@ describe('Update User Endpoint', () => {
       expect(typeof response._body.job).toBe('string')
       expect(typeof response._body.updatedAt).toBe('string')
   });
-});
-
+});*/
