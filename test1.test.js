62979e8c0003dcd8d4f448f6df318fcd5e18a8d1
mablebiju <52173815+mablebiju@users.noreply.github.com>
2023-11-06 08:56:35 +0000

    Update test1.test.js

--- test1.test.js
+++ test1.test.js
@@ -1,3 +1,4 @@
+//Test Case 1 – Create User – log in – delete user
 import { expect } from "@playwright/test";
 import request from "supertest";
 
@@ -62,43 +63,4 @@ describe('Delete endpoint', () => {
 	});
 })
 
-/*
-describe('Single User Endpoint', () => {
-	it('should return a 200 status code and valid response body', async () => {
-		const response = await request(baseUrl)
-			.get('users/2')
-      .set('Content-Type', 'application/json')
-			.set('Accept', 'application/json');
 
-    //console.log("response", response)  
-		expect(response.statusCode).toBe(200);
-    expect(typeof response._body.data.id).toBe('number')
-    expect(typeof response._body.data.email).toBe('string')
-    expect(typeof response._body.data.first_name).toBe('string')
-    expect(typeof response._body.data.last_name).toBe('string')
-    expect(typeof response._body.data.avatar).toBe('string')
-    expect(typeof response._body.support.url).toBe('string')
-    expect(typeof response._body.support.text).toBe('string')
-	});
-});
-
-const payload = {
-  name: "morpheus",
-  job: "zion resident"
-};
-
-describe('Update User Endpoint', () => {
-  it('should return a 200 status code and valid response body after updating user', async () => {
-      const response = await request(baseUrl)
-          .put('users/2')
-          .send(payload)
-          .set('Content-Type', 'application/json')
-          .set('Accept', 'application/json');
-      //console.log("response", response)
-      expect(response.statusCode).toBe(200);
-      expect(response.body).toBeDefined();
-      expect(typeof response._body.name).toBe('string')
-      expect(typeof response._body.job).toBe('string')
-      expect(typeof response._body.updatedAt).toBe('string')
-  });
-});*/
